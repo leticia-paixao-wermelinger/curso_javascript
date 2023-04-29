74c0bc30efb8da0fb64ce2e04e04c6d110fe0673
Letícia Paixão <leticiapwermelinger@gmail.com>
2023-04-28 22:40:36 -0300

    Aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+console.log('Teste: O console funcionou corretamente')
+var vel = 12.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // Condução simples. Não tem 'senão'
+    console.log('Você ultrapasosu a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
