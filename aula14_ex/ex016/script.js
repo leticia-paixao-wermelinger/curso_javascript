ef5a395f553c10a03b61b606f615662c01c2dc12
Letícia Paixão <leticiapwermelinger@gmail.com>
2023-06-20 20:33:33 -0300

    ex 16

--- aula14_ex/ex016/script.js
+++ aula14_ex/ex016/script.js
@@ -1,22 +1,39 @@
 function carregar() {
-    var horario = new Date().getHours()
-    window.document.querySelector('#hora').innerHTML = `Agora são ${horario} horas.`
-    var img = window.document.getElementById('imagem')
+    var inicio = Number(window.document.querySelector('#start').value)
+    var final = Number(window.document.querySelector('#ending').value)
+    var distancia = Number(window.document.querySelector('#step').value)
 
-    if (horario >= 6 && horario < 12){
-        img.src = 'imagens/manha.png'
-        document.body.style.background = '#fed28b'
-    } else if (horario >= 0 && horario < 6) {
-        img.src = 'imagens/madrugada.png'
-        document.body.style.background = '#81598d'
-    } else if (horario > 18) {
-        img.src = 'imagens/noite.png'
-        document.body.style.background = '#9e666f'
-    } else if (horario == 18) {
-        img.src = 'imagens/anoitecer.png'
-        document.body.style.background = '#974e48'
+    if (inicio.length == 0 || final.length == 0 || distancia.length == 0) {
+        window.document.getElementById('contagem').innerHTML = 'Por favor, preencha todos os espaços em branco para realizar a contagem.'
     } else {
-        img.src = 'imagens/tarde.png'
-        document.body.style.background = '#72a0d4'
+        window.document.getElementById('contagem').innerHTML = `Você solicitou a seguinte contagem: O número inicial é ${inicio}, o final é ${final} e os números vão pular de ${distancia} em ${distancia}.`
+        
+        if (inicio < final){
+            for (var n = inicio ; n <= final ; n += distancia){ //Contagem crescente
+                window.document.querySelector('#resultado').innerHTML += ` ${n} &#9998; ` // No vídeo explicativo, o Prof. ensinou o código unicode /u{código} para colocar um símbolo, mas eu havia feito aqui com o código do HTML e funcionou. Está errado?
+            }    
+        } else {
+            for (var n = inicio ; n >= final ; n -= distancia){ // Contagem regressiva
+                window.document.querySelector('#resultado').innerHTML += ` ${n} &#9998; ` // No vídeo explicativo, o Prof. ensinou o código unicode /u{código} para colocar um símbolo, mas eu havia feito aqui com o código do HTML e funcionou. Está errado?
+            }    
+        }
+        /*
+        if (distancia.length == 0){
+            distancia = '1'
+            __
+        } else {
+            __
+            }
+        }*/
     }
 }
+
+/*
+    Pontos para lembrar:
+        1. Eu sempre esqueço de transformar um número, com o Number()
+        2. Se eu já coloquei .value na variável, não precisa repetir
+        3. Gravar / Entender melhor, na prática, a diferença entre = e ==
+        4. Lembrar da existência do += para incluir algo no parágrafo sem apagar o que já estava antes!
+        5. Eu ainda não consegui entender como funciona o .write. Toda vez que tendo utilizar dá erro... 
+        6. Para utilizar .length a variável que vem antes do ponto não pode ter sido alterada para Number()
+*/
